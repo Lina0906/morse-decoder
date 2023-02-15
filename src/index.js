bf529db3d084a21caafec166212f16aea171a8cb
Lina Churbanova <lina.moiseeva@gmail.com>
2023-02-15 23:37:49 +0400

    fix: fix decode function

--- src/index.js
+++ src/index.js
@@ -37,10 +37,33 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+MORSE_TABLE['**********'] = " ";
+
 function decode(expr) {
     // write your solution here
+    const binMorseTable = {};
+  for (let key in MORSE_TABLE) {
+    let binKey = key.replaceAll(".", "10").replaceAll("-", "11");
+
+    if (binKey.length < 10) { binKey = '0'.repeat(10 - binKey.length) + binKey };    
+
+    binMorseTable[binKey] = MORSE_TABLE[key];    
+  };
+  
+  const exprArr = [];
+
+  for (let i = 0; i < expr.length; i += 10) {
+    exprArr.push(expr.slice(i, i + 10));
+  }; 
+
+  const resultArr = exprArr.map((element) => {
+   return binMorseTable[element];
+  });
+
+  return resultArr.join('');
 }
 
 module.exports = {
     decode
 }
+
